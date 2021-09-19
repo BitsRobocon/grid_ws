95da141c4a7065f6a30bb9ae4a7cd7068fc35ddf
Ekansh Gupta <ekanshgupta92@gmail.com>
2021-09-19 22:20:33 +0530

    Added pose calculator

--- src/aruco_ros/scripts/detect_state_ros.py
+++ src/aruco_ros/scripts/detect_state_ros.py
@@ -11,6 +11,7 @@ from imutils.video import VideoStream
 # from utils import ARUCO_DICT 
 from geometry_msgs.msg import Pose, Twist
 import copy
+import math
 # Replace aruco_ros by whatever package name you have given.
 from aruco_ros.msg import BotPose, BotTwist
 from aruco_ros.srv import SrvPose, SrvPoseRequest, SrvPoseResponse, SrvTwist, SrvTwistRequest, SrvTwistResponse
@@ -29,6 +30,8 @@ class detect_aruco():
         self.rate = rospy.Rate(10)
         print("Detect Aruco class initialised!")
         self.arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_100)
+        self.matrix_coefficients = np.load('calibration_matrix.npy')
+        self.distortion_coefficients = np.load('distortion_coefficients.npy')
         self.arucoParams = cv2.aruco.DetectorParameters_create()
         self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 
@@ -63,6 +66,55 @@ class detect_aruco():
         self.vs = VideoStream(src=0).start()
         time.sleep(3)
 
+    def isclose(self, x, y, rtol=1.e-5, atol=1.e-8):
+        return abs(x-y) <= atol + rtol * abs(y)
+
+    def euler_angles_from_rotation_matrix(self, R):
+        '''
+        From a paper by Gregory G. Slabaugh (undated),
+        "Computing Euler angles from a rotation matrix
+        '''
+        phi = 0.0
+        if self.isclose(R[2,0],-1.0):
+            theta = math.pi/2.0
+            psi = math.atan2(R[0,1],R[0,2])
+        elif self.isclose(R[2,0],1.0):
+            theta = -math.pi/2.0
+            psi = math.atan2(-R[0,1],-R[0,2])
+        else:
+            theta = -math.asin(R[2,0])
+            cos_theta = math.cos(theta)
+            psi = math.atan2(R[2,1]/cos_theta, R[2,2]/cos_theta)
+            phi = math.atan2(R[1,0]/cos_theta, R[0,0]/cos_theta)
+        # return psi, theta, phi      #roll, pitch, yaw
+        return phi
+
+    def pose_esitmation(self, frame):
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.aruco_dict = cv2.aruco.Dictionary_get(self.arucoDict)
+        parameters = cv2.aruco.DetectorParameters_create()
+
+        corners, ids, rejected_img_points = cv2.aruco.detectMarkers(gray, cv2.aruco_dict,parameters=parameters,
+            cameraMatrix = self.matrix_coefficients,
+            distCoeff = self.distortion_coefficients)
+
+        # If markers are detected
+        if len(corners) > 0:
+            for i in range(0, len(ids)):
+                # Estimate pose of each marker and return the values rvec and tvec---(different from those of camera coefficients)
+                rvec, tvec, markerPoints = cv2.aruco.estimatePoseSingleMarkers(corners[i], 0.02, self.matrix_coefficients,
+                                                                           self.distortion_coefficients)
+                # Draw a square around the markers
+                cv2.aruco.drawDetectedMarkers(frame, corners) 
+                # print(tvec[0][0][0], tvec[0][0][1], tvec[0][0][2]) #x,y,z 
+                R_mat, _ = cv2.Rodrigues(rvec)
+                yaw = self.euler_angles_from_rotation_matrix(R_mat) 
+
+                # Draw Axis
+                cv2.aruco.drawAxis(frame, self.matrix_coefficients, self.distortion_coefficients, rvec, tvec, 0.01)  
+
+        return frame , tvec[0][0][0], yaw 
+
     def service_pose(self, request):
         self.pose_response.header.stamp = self.time
         self.pose_response.pose = [self.bot1.curr_pose, self.bot2.curr_pose, self.bot3.curr_pose, self.bot4.curr_pose]
@@ -87,6 +139,7 @@ class detect_aruco():
         
         # detect ArUco markers in the input frame
         (corners, ids, rejected) = cv2.aruco.detectMarkers(image,self.arucoDict,parameters=self.arucoParams)
+        _ , x_pos, yaw = self.euler_angles_from_rotation_matrix(frame, self.aruco_dict_type, self.k, self.d)
 
         if len(corners) > 0:
             # flatten the ArUco IDs list
@@ -126,35 +179,35 @@ class detect_aruco():
                 else:
                     self.flag = self.flag + 1 
                     if markerID == 0:
-                        self.bot1.curr_pose.position.x = cX
-                        self.bot1.curr_pose.position.y = cY
+                        self.bot1.curr_pose.position.x = x_pos
+                        self.bot1.curr_pose.position.y = yaw    # didn't know for angular so used this :p
                         # if (self.delta_t > 2):
                         self.bot1.curr_twist.linear.x = (self.bot1.curr_pose.position.x - self.bot1.prev_pose.position.x)/self.delta_t
-                        self.bot1.curr_twist.linear.y = (self.bot1.curr_pose.position.y - self.bot1.prev_pose.position.y)/self.delta_t
+                        self.bot1.curr_twist.angular.z = (self.bot1.curr_pose.position.y - self.bot1.prev_pose.position.y)/self.delta_t
                         self.bot1.prev_pose = copy.deepcopy(self.bot1.curr_pose)
 
                     elif markerID == 1:
-                        self.bot2.curr_pose.position.x = cX
-                        self.bot2.curr_pose.position.y = cY
+                        self.bot2.curr_pose.position.x = x_pos
+                        self.bot2.curr_pose.position.y = yaw
                         # if (self.delta_t > 2):
                         self.bot2.curr_twist.linear.x = (self.bot2.curr_pose.position.x - self.bot2.prev_pose.position.x)/self.delta_t
-                        self.bot2.curr_twist.linear.y = (self.bot2.curr_pose.position.y - self.bot2.prev_pose.position.y)/self.delta_t
+                        self.bot2.curr_twist.angular.z = (self.bot2.curr_pose.position.y - self.bot2.prev_pose.position.y)/self.delta_t
                         self.bot2.prev_pose = copy.deepcopy(self.bot2.curr_pose)
 
                     elif markerID == 2:
-                        self.bot3.curr_pose.position.x = cX
-                        self.bot3.curr_pose.position.y = cY
+                        self.bot3.curr_pose.position.x = x_pos
+                        self.bot3.curr_pose.position.y = yaw
                         # if (self.delta_t > 2):
                         self.bot3.curr_twist.linear.x = (self.bot3.curr_pose.position.x - self.bot3.prev_pose.position.x)/self.delta_t
-                        self.bot3.curr_twist.linear.y = (self.bot3.curr_pose.position.y - self.bot3.prev_pose.position.y)/self.delta_t
+                        self.bot3.curr_twist.angular.z = (self.bot3.curr_pose.position.y - self.bot3.prev_pose.position.y)/self.delta_t
                         self.bot3.prev_pose = copy.deepcopy(self.bot3.curr_pose)
 
                     elif markerID == 3:
-                        self.bot4.curr_pose.position.x = cX
-                        self.bot4.curr_pose.position.y = cY
+                        self.bot4.curr_pose.position.x = x_pos
+                        self.bot4.curr_pose.position.y = yaw
                         # if (self.delta_t > 2):
                         self.bot4.curr_twist.linear.x = (self.bot4.curr_pose.position.x - self.bot4.prev_pose.position.x)/self.delta_t
-                        self.bot4.curr_twist.linear.y = (self.bot4.curr_pose.position.y - self.bot4.prev_pose.position.y)/self.delta_t
+                        self.bot4.curr_twist.angular.z = (self.bot4.curr_pose.position.y - self.bot4.prev_pose.position.y)/self.delta_t
                         self.bot4.prev_pose = copy.deepcopy(self.bot4.curr_pose)
 
             if self.delta_t > 2:
